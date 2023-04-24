13cd7037ba1a87e3f0f982877aefbd31d5bfd462
UnnamedPixel <apinel@insa-toulouse.fr>
2023-04-24 16:37:36 +0200

    fusion Motor & BLED

--- MMs_dispenser.ino
+++ MMs_dispenser.ino
@@ -1,26 +1,29 @@
 #include "MMS_dispenser.h"
-<<<<<<< HEAD
 #include "BLED.cpp"
-#define BLED_DATA_PIN D6
-
-BLED<BLED_DATA_PIN> Banc(BLED_DATA_PIN,6);
-vector<int> tabint1  = {0xEE82EE, CRGB::Yellow, 0xEE82EE, CRGB::Yellow, 0xEE82EE, CRGB::Yellow};
-vector<int> tabint2  = {CRGB::Yellow, 0xEE82EE, CRGB::Yellow, 0xEE82EE, CRGB::Yellow, 0xEE82EE};
-=======
 #include "motor.cpp"
 
+#define BLED_DATA_PIN D6
 #define MOTOR_DATA_PIN D7
 
-Motor<MOTOR_DATA_PIN> theMOTOR(D7);
->>>>>>> MotorDONE
+
+BLED<BLED_DATA_PIN> Banc(BLED_DATA_PIN,6);
+Motor<MOTOR_DATA_PIN> theMOTOR(MOTOR_DATA_PIN);
+
+
+vector<int> tabint1  = {0xEE82EE, CRGB::Yellow, 0xEE82EE, CRGB::Yellow, 0xEE82EE, CRGB::Yellow};
+vector<int> tabint2  = {CRGB::Yellow, 0xEE82EE, CRGB::Yellow, 0xEE82EE, CRGB::Yellow, 0xEE82EE};
+
 
 void setup() {
+  Banc.setup();
   theMOTOR.setup();
 }
 
 void loop() {
   theMOTOR.move2pos(120);
+  Banc.display(tabint1);
   delay(2000);
   theMOTOR.move2pos(20);
+  Banc.display(tabint2);
   delay(2000);
 }
