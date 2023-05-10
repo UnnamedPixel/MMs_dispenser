816271718e5c30f0dea2bb1cfe8fc83b7c91a46a
UnnamedPixel <apinel@insa-toulouse.fr>
2023-05-10 20:02:24 +0200

    sauv av fusion

--- Melodies.cpp
+++ Melodies.cpp
@@ -1,6 +1,6 @@
 #include "Melodies.h"
 
-int AAA[5] = {NOTE_C4,NOTE_A3,NOTE_G3,NOTE_E3,NOTE_C3};
-int BBB[5] = {150,150,150,150,150};
-int CCC[5] = {100,100,100,100,100};
-Melody Loser( 5,  AAA,  BBB, CCC);
+int Loser_notes[5] = {NOTE_C4,NOTE_A3,NOTE_G3,NOTE_E3,NOTE_C3};
+int Loser_duration[5] = {150,150,150,150,150};
+int Loser_delay[5] = {100,100,100,100,100};
+Melody Loser( 5,  Loser_notes,  Loser_duration, Loser_delay);
